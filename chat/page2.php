c0371a2999e6dd1d7652cc959213db4cb7d1f4ff
rawan-mortaja <rawanmortaja077@gmail.com>
2023-03-06 10:24:07 +0200

    Rawan

--- chat/page2.php
+++ chat/page2.php
@@ -1,5 +1,5 @@
 <?php
-// Here the variables are set and a connection is made to the database
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -12,10 +12,11 @@ if ($conn->connect_error) {
 }
 // This sql inserts the messages into the database, it adds to messages, all are saved can be displayed
 $sql = "INSERT INTO chat (msg) VALUES ('$input')";
+// var_dump($sql);
 //This updates a single line of the database only one message will be saved and overwritten
 // $sql = "UPDATE chat SET msg= '$input' WHERE ID = '$id'";
 // This bit of code shows that if the connection is successful, the user is shown the message - if not, an error is shown
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql) == TRUE) {
     header("location: index.php");
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
